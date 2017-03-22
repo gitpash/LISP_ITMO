7cdca0e3f7ccf548493e0fb13b3e389f06ebde01
gitpash <arklogin@gmail.com>
2017-03-22 23:52:03 +0300

     'continue week4'

--- week4/odd-val.lisp
+++ week4/odd-val.lisp
@@ -1,8 +1,8 @@
 (defun count-odd (lst)
+     ; считаем нечетные числа в списке
     (let ((count 0) (odds-list nil))
         (dolist (i lst)
             (when (oddp i)
                 (push i odds-list)
                 (incf count)))
         (values count (nreverse odd-list))))
-        
